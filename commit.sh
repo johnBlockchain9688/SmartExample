ce6676bea2faca122d18f0b5d0a244ffb2c00d48
developer <john.blockchain9688@gmail.com>
2023-12-27 17:59:03 +0100

    test

---
+++ commit.sh
@@ -0,0 +1,7 @@
+git remote remove origin
+#git remote set-url --add origin  git@github.com:johnBlockchain9688/SmartExample.git
+
+ git remote add origin git@github.com:johnBlockchain9688/SmartExample.git
+git add -A
+git commit -m 
+git push -u origin main 
