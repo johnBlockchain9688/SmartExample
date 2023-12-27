fb6ccad07899d6d6e795ccdc646bc0edc4a4c377
developer <john.blockchain9688@gmail.com>
2023-12-27 18:33:23 +0100

    Commit changes

--- commit.sh
+++ commit.sh
@@ -1,7 +1,16 @@
 git remote remove origin
-#git remote set-url --add origin  git@github.com:johnBlockchain9688/SmartExample.git
 
- git remote add origin git@github.com:johnBlockchain9688/SmartExample.git
+git remote add origin git@github.com:johnBlockchain9688/SmartExample.git
+
+#First get all changes from remote
+git pull origin main
+
+#Add new file
 git add -A
-git commit -m 
+
+#Commit changes
+git commit -m "Commit changes" 
+
+
+#Push changes to origin
 git push -u origin main 
